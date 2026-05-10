d8877676218d5f97114be397f4c5e3f7802ea8d0
Tehmina Khan <tehminakhan9399@gmail.com>
2026-05-10 14:42:28 +0500

    nodemailer port update

--- server/services/alerts.js
+++ server/services/alerts.js
@@ -4,11 +4,21 @@ const nodemailer = require("nodemailer");
 const twilio     = require("twilio");
 const User       = require("../models/User");
 
+// const transporter = nodemailer.createTransport({
+//   host: "smtp.gmail.com",
+//   port: 587,
+//   secure: false, // TLS
+//   family: 4,     // FORCE IPv4
+//   auth: {
+//     user: process.env.EMAIL_USER,
+//     pass: process.env.EMAIL_PASS,
+//   },
+// });
+
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 587,
-  secure: false, // TLS
-  family: 4,     // FORCE IPv4
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
